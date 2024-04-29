3c84809cffd3a09b22340aed02a7b9366cdd4478
Zidan <zidanlastone01@gmail.com>
2024-04-29 08:27:52 +0700

    delete kode_finger requirement

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'nama_pegawai',
-        // 'kode_finger',
         'email',
         'password',
         'level',
